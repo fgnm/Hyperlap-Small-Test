f20f3a67ec9bc676f6f6b045f3c27143c7fdbe39
fgnm <fgnm.dev@gmail.com>
2023-02-25 15:18:33 +0100

    Add switch project

---
+++ switch/src/main/java/com/test/switchgdx/SwitchLauncher.java
@@ -0,0 +1,11 @@
+package com.test.switchgdx;
+
+import com.test.GameMain;
+import com.thelogicmaster.switchgdx.SwitchApplication;
+
+public class SwitchLauncher {
+
+	public static void main (String[] arg) {
+		new SwitchApplication(new GameMain());
+	}
+}
